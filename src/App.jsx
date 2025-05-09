9b6671ae3e4f05d3b4cf21877d64d241103c1533
harshad8180 <hpborkar@gmail.com>
2025-05-10 00:41:00 +0530

    rendering the conents based on conditional

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
     <div className="app-container">
       <Sidebar />
       <div className="content">
         <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
 
-        <CreatePost />
-        <PostList />
         <Footer />
       </div>
     </div>
