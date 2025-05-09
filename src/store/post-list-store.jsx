fd5b696d9a90b62917c9827b9b63531ef88c87d8
harshad8180 <hpborkar@gmail.com>
2025-05-10 02:21:35 +0530

    passing the dummy posts via props using hooks useContext

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {},
@@ -11,7 +11,10 @@ const postListReducer = (currPostList, action) => {
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(postListReducer, []);
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
   const addPost = () => {};
 
@@ -23,4 +26,23 @@ const PostListProvider = ({ children }) => {
     </PostList.Provider>
   );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Paas ho gaya bhai",
+    body: "4 saal ki masti ke baad bhi ho gaye hain paas. Hard to believe.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable", "Happy"],
+  },
+];
 export default PostListProvider;
